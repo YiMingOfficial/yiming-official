14291a18f299c9995154b8e6c78227682e8c5759
YiMingOfficial <126741802+YiMingOfficial@users.noreply.github.com>
2023-03-02 13:04:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-<script language="php">
-    $x=8;
-    $y=1;
-    $total=$x+$y;
-    echo "".$total;
-</script>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	echo "My first PHP script!";
+?>
+
+</html>
+</body>
